4093130bb2bd099f68a23df2700a3d3996a42daa
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-07-18 23:19:31 +0530

    Add files via upload
    Suppose we have one another controller and that controller want to access some other Layout but as we had mention one layout in _Viewstart.cshtml then it will access that same Layout  so to overcome this problem 
    We have two ways
    1st ---- While Adding View we can select that Layout by going to that Layout 
    
    2nd option is 
     public ActionResult Index()
            {
                return View("Index", "_adminLayout");
            }	
    Or go to Admin  index page and mention  the full path
    @{
    Layout=“~/Views/Shared/_adminlayout.cshtml”;
    }
    
    
    Rather Than Writing the Path in Each View we can just make a _ViewStart.cshtml  View in  View Folder as Razor Engine  first check  in _ViewStart.cshtml then it go to  Shared View
    So in _ViewStart.Cshtml just mention the path and remove path from each Action View
    @{
    Layout=“~/Views/Shared/_mylayout.cshtml”;
    }

---
+++ HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [Route("programs")]
+        public ActionResult Programs()
+        {
+            return View();
+        }
+
+    }
+}
